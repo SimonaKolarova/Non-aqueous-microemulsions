a1cb1d0e84d20592ebe7847df01da73dd941b03d
Simona Kolarova <61417841+SimonaKolarova@users.noreply.github.com>
2021-04-10 17:10:18 +0100

    Feature selection comparison

---
+++ Feature selection/Comparison of feature selection methods/Predictive performance assessment.R
@@ -0,0 +1,66 @@
+# Predictive performance assessement
+model_performance_assessment <- function(training_data, testing_data, validation_data, predictors, model_performance, model, model_name, model_number) {
+  
+  # Training data metrics
+  auc_training <- as.numeric(auc(
+    roc(
+      predict(model,training_data[,predictors]), 
+      as.numeric(training_data$Phase.behaviour))))
+  
+  confusion_matrix_training <- confusionMatrix(
+    predict(model, training_data[,predictors]),
+    training_data$Phase.behaviour, 
+    positive = "Microemulsion")
+  
+  
+  # Testing data metrics
+  auc_testing <- as.numeric(auc(
+    roc(
+      predict(model,testing_data[,predictors]), 
+      as.numeric(testing_data$Phase.behaviour))))
+  
+  confusion_matrix_testing <- confusionMatrix(
+    predict(model, testing_data[,predictors]),
+    testing_data$Phase.behaviour, 
+    positive = "Microemulsion")
+  
+  # Validation data metrics
+  auc_validation <- as.numeric(auc(
+    roc(
+      predict(model,validation_data[,predictors]), 
+      as.numeric(validation_data$Phase.behaviour))))  
+  
+  confusion_matrix_validation <- confusionMatrix(
+    predict(model, validation_data[,predictors]),
+    validation_data$Phase.behaviour, 
+    positive = "Microemulsion")
+  
+  # Add to model_performance dataframe
+  model_performance$Model[model_number] <- model_name
+  
+  model_performance$AUC_training[model_number] <- auc_training
+  model_performance$Accuracy_training[model_number] <- as.numeric(confusion_matrix_training[[3]]["Accuracy"])
+  model_performance$Kappa_training[model_number] <- as.numeric(confusion_matrix_training[[3]]["Kappa"])
+  model_performance$Sensitivity_training[model_number] <- as.numeric(confusion_matrix_training[[4]]["Sensitivity"])
+  model_performance$Specificity_training[model_number] <- as.numeric(confusion_matrix_training[[4]]["Specificity"])
+  model_performance$Precision_training[model_number] <- as.numeric(confusion_matrix_training[[4]]["Precision"])
+  model_performance$Fscore_training[model_number] <- as.numeric(confusion_matrix_training[[4]]["F1"])
+  
+  model_performance$AUC_testing[model_number] <- auc_testing
+  model_performance$Accuracy_testing[model_number] <- as.numeric(confusion_matrix_testing[[3]]["Accuracy"])
+  model_performance$Kappa_testing[model_number] <- as.numeric(confusion_matrix_testing[[3]]["Kappa"])
+  model_performance$Sensitivity_testing[model_number] <- as.numeric(confusion_matrix_testing[[4]]["Sensitivity"])
+  model_performance$Specificity_testing[model_number] <- as.numeric(confusion_matrix_testing[[4]]["Specificity"])
+  model_performance$Precision_testing[model_number] <- as.numeric(confusion_matrix_testing[[4]]["Precision"])
+  model_performance$Fscore_testing[model_number] <- as.numeric(confusion_matrix_testing[[4]]["F1"])
+  
+  model_performance$AUC_validation[model_number] <- auc_validation
+  model_performance$Accuracy_validation[model_number] <- as.numeric(confusion_matrix_validation[[3]]["Accuracy"])
+  model_performance$Kappa_validation[model_number] <- as.numeric(confusion_matrix_validation[[3]]["Kappa"])
+  model_performance$Sensitivity_validation[model_number] <- as.numeric(confusion_matrix_validation[[4]]["Sensitivity"])
+  model_performance$Specificity_validation[model_number] <- as.numeric(confusion_matrix_validation[[4]]["Specificity"])
+  model_performance$Precision_validation[model_number] <- as.numeric(confusion_matrix_validation[[4]]["Precision"])
+  model_performance$Fscore_validation[model_number] <- as.numeric(confusion_matrix_validation[[4]]["F1"])
+  
+  return(model_performance)
+}
